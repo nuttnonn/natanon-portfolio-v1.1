1d7c1fd81b891d4244af81f5d78ab8de714b0c3d
Natanon Trangratanajit <nutt.nonn@gmail.com>
2023-10-26 00:17:41 +0700

    added revalidate time

--- src/app/api/recently-played/route.js
+++ src/app/api/recently-played/route.js
@@ -1,6 +1,6 @@
 import { recentlyPlayed } from '../../lib/spotify'
 
-export const runtime = 'edge';
+// export const runtime = 'edge';
 
 export async function GET() {
   const response = await recentlyPlayed();
